f583f3db5e34d10fc4b7386138b3e8038eb04f4b
Lê Trung Hiếu ( Henry Le ) <lthieu1599@gmail.com>
2022-08-24 22:08:16 +0700

    + Add animation script

--- Blocky Block/Assets/Scripts/Core/UnitAnimation.cs
+++ Blocky Block/Assets/Scripts/Core/UnitAnimation.cs
@@ -6,6 +6,16 @@ namespace BlockyBlock.Core
 {
     public class UnitAnimation : MonoBehaviour
     {
+        [SerializeField] private Animator m_Animator;
         private static readonly int MovingKeyAnimation = Animator.StringToHash("Moving");
+        private static readonly int BlendKeyAnimation = Animator.StringToHash("Blend");
+        public void TriggerMovingBool(bool _status)
+        {
+            m_Animator.SetBool(MovingKeyAnimation, _status);
+        }
+        public void TriggerBlendValue(float _value)
+        {
+            m_Animator.SetFloat(BlendKeyAnimation, _value);
+        }
     }
 }
