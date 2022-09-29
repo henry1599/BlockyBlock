0c46e4f74cc778f3f13887d7190e869f01a69c87
Lê Trung Hiếu ( Henry Le ) <lthieu1599@gmail.com>
2022-09-30 00:51:45 +0700

    * [MAP GENERATOR] Finish generate string from input enum

--- Blocky Block/Assets/Scripts/Editor/MapGenerator.cs
+++ Blocky Block/Assets/Scripts/Editor/MapGenerator.cs
@@ -1,12 +1,23 @@
 #if UNITY_EDITOR
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.UIElements;
 #endif
 using UnityEngine;
 using UnityEngine.UIElements;
+using BlockyBlock.Enums;
 
 namespace BlockyBlock.Editor 
 {
+    public enum GroundTypeEditor {
+        Ground = 0,
+        Water = 1,
+        Tree = 2,
+        Box = 3,
+        None = 4,
+        Trap = 5,
+        Star = 6
+    }
     public class MapGenerator : EditorWindow
     {
         [SerializeField] VisualTreeAsset m_Tree;
@@ -15,8 +26,9 @@ namespace BlockyBlock.Editor
         private Button m_InitButton;
         private Button m_GenerateButton;
         private IMGUIContainer m_ButtonContainer;
-        float m_ButtonWidth = 50;
+        float m_ButtonWidth = 65;
         float m_ButtonHeight = 20;
+        List<EnumField> m_Enums = new List<EnumField>();
 
 
         [MenuItem("Tools/Map Generator")]
@@ -35,34 +47,107 @@ namespace BlockyBlock.Editor
             m_GenerateButton = rootVisualElement.Q<Button>("_generateBtn");
 
             m_InitButton.clicked += GenerateButton;
+            m_GenerateButton.clicked += HandleGenerateBtnClicked;
+        }
+        void OnDestroy() 
+        {
+            m_InitButton.clicked -= GenerateButton;  
+            m_GenerateButton.clicked -= HandleGenerateBtnClicked;  
         }
         void GenerateButton()
         {
+            foreach (EnumField e in m_Enums)
+            {
+                e.UnregisterCallback<ChangeEvent<System.Enum>>(evt =>
+                {
+                    e.name = GetCharByGroundTypeEditor((GroundTypeEditor)evt.newValue);
+                });
+            }
+            m_Enums.Clear();
             m_ButtonContainer.Clear();
             int numberOfButtons = (int)m_WidthSlider.value * (int)m_HeightSlider.value;
             m_ButtonContainer.style.width = m_ButtonWidth * m_WidthSlider.value;
             m_ButtonContainer.style.height = m_ButtonHeight * m_HeightSlider.value;
             for (int i = 0; i < numberOfButtons; i++)
             {
-                m_ButtonContainer.Add(CreateButton());
+                m_ButtonContainer.Add(CreateEnum());
             }
         }
-        Button CreateButton()
+        EnumField CreateEnum()
         {
-            Button btn = new Button();
-            btn.style.width = 50;
-            btn.style.height = 20;
-            btn.text = "Ground";
-            btn.style.marginLeft = 0;
-            btn.style.marginRight = 0;
-            btn.style.marginBottom = 0;
-            btn.style.marginTop = 0;
-            btn.style.paddingLeft = 0;
-            btn.style.paddingRight = 0;
-            btn.style.paddingBottom = 0;
-            btn.style.paddingTop = 0;
+            EnumField e = new EnumField(GroundTypeEditor.Ground);
+            e.style.width = m_ButtonWidth;
+            e.style.height = m_ButtonHeight;
+            e.label = "";
+            e.style.marginLeft = 0;
+            e.style.marginRight = 0;
+            e.style.marginBottom = 0;
+            e.style.marginTop = 0;
+            e.style.paddingLeft = 0;
+            e.style.paddingRight = 0;
+            e.style.paddingBottom = 0;
+            e.style.paddingTop = 0;
+            e.name = "0";
+            
+            e.RegisterCallback<ChangeEvent<System.Enum>>(evt =>
+            {
+                e.name = GetCharByGroundTypeEditor((GroundTypeEditor)evt.newValue);
+            });
 
-            return btn;
+            m_Enums.Add(e);
+
+            return e;
+        }
+        void HandleGenerateBtnClicked()
+        {
+            int width = m_WidthSlider.value; // 2
+            int height = m_HeightSlider.value; // 3
+            // 00
+            // 00
+            // 00
+
+            string resultString = "";
+
+            for (int i = 0; i < height; i++)
+            {
+                // i = 0, 1
+                // Debug.Log("i : " + i.ToString());
+                string stringEachRow = "";
+                for (int j = 0; j < width; j++)
+                {
+                    // j = 0 -> 1
+                    // j = 2 -> 
+                    // Debug.Log(j);
+                    string split = m_Enums[i * width + j].name;
+                    // Debug.Log("e : " + split);
+                    stringEachRow += split;
+                }
+                stringEachRow += ";";
+                resultString += stringEachRow;
+            }
+            Debug.Log(resultString);
+        }
+        string GetCharByGroundTypeEditor(GroundTypeEditor _type)
+        {
+            switch (_type)
+            {
+                case GroundTypeEditor.Ground:
+                    return "0";
+                case GroundTypeEditor.Water:
+                    return "1";
+                case GroundTypeEditor.Tree:
+                    return "2";
+                case GroundTypeEditor.Box:
+                    return "3";
+                case GroundTypeEditor.None:
+                    return "4";
+                case GroundTypeEditor.Trap:
+                    return "5";
+                case GroundTypeEditor.Star:
+                    return "6";
+                default:
+                    return "4";
+            }
         }
     }
 }
