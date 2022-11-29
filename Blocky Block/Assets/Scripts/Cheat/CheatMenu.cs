869d7fdb28443695fe348b98378e5e23645bde39
Lê Trung Hiếu ( Henry Le ) <lthieu1599@gmail.com>
2022-11-29 19:29:34 +0700

    + Add buttons in local data cheat menu

--- Blocky Block/Assets/Scripts/Cheat/CheatMenu.cs
+++ Blocky Block/Assets/Scripts/Cheat/CheatMenu.cs
@@ -8,12 +8,35 @@ namespace BlockyBlock.Cheat
 {
     public class CheatMenu : MonoBehaviour
     {
-        [Header("Tab Buttons")]
-        [SerializeField] Button localDataButton, consoleButton;
-
-        [Space(5)]
-        [Header("Contents")]
-        [SerializeField] GameObject locaDataContent;
-        [SerializeField] DebugLogManager consoleContent;
+        [Space(10)]
+        [Header("Local Data Content")]
+        [SerializeField] Button resetLocalDataButton;
+        [SerializeField] Button resetUserProfileButton;
+        [SerializeField] Button resetAccountButton;
+        void OnEnable()
+        {
+            this.resetLocalDataButton.onClick.AddListener(() => OnResetLocalDataButtonClick());
+            this.resetUserProfileButton.onClick.AddListener(() => OnResetUserProfileButtonClick());
+            this.resetAccountButton.onClick.AddListener(() => OnResetAccountButtonClick());
+        }
+        void OnDisable()
+        {
+            this.resetLocalDataButton.onClick.RemoveListener(() => OnResetLocalDataButtonClick());
+            this.resetUserProfileButton.onClick.RemoveListener(() => OnResetUserProfileButtonClick());
+            this.resetAccountButton.onClick.RemoveListener(() => OnResetAccountButtonClick());
+        }
+        void OnResetLocalDataButtonClick()
+        {
+            PlayerPrefs.DeleteAll();
+        }
+        void OnResetUserProfileButtonClick()
+        {
+            PlayerPrefs.DeleteKey(Events.GameConstants.PROFILE_KEY);
+        }
+        void OnResetAccountButtonClick()
+        {
+            PlayerPrefs.DeleteKey(BackEnd.BEConstants.ACCESS_TOKEN_KEY);
+            PlayerPrefs.DeleteKey(BackEnd.BEConstants.REFRESH_TOKEN_KEY);
+        }
     }
 }
